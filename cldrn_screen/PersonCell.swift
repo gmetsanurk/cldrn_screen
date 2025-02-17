27bc469c02ff283bfac4f84be36c15827e7b42f8
Georgy Metsanurk <gmetsanurk@gmail.com>
2025-02-17 18:41:19 +0300

    work with PersonCell

--- cldrn_screen/PersonCell.swift
+++ cldrn_screen/PersonCell.swift
@@ -1,14 +1,45 @@
 import UIKit
 
 class PersonCell: UICollectionViewCell {
+    
     let nameTextField = UITextField()
     let ageTextField = UITextField()
     
+    private let stackView: UIStackView = {
+            let stack = UIStackView()
+            stack.axis = .vertical
+            stack.spacing = 8
+            return stack
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        nameTextField.placeholder = "Имя"
+        ageTextField.placeholder = "Возраст"
+        ageTextField.keyboardType = .numberPad
+        
+        stackView.addArrangedSubview(nameTextField)
+        stackView.addArrangedSubview(ageTextField)
+        
+        contentView.addSubview(stackView)
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10)
+        ])
     }
 }

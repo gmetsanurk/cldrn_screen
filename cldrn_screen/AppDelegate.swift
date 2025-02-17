907b684beb98c43b67e7ea1839161e444f4debff
Georgy Metsanurk <gmetsanurk@gmail.com>
2025-02-17 17:57:30 +0300

    create project

---
+++ cldrn_screen/AppDelegate.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(
+        _: UIApplication,
+        didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        setupWindow()
+        return true
+    }
+    
+    private func setupWindow() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let homeVC = HomeView()
+        //let navigationController = UINavigationController(rootViewController: homeVC)
+        window?.rootViewController = homeVC//navigationController
+        window?.makeKeyAndVisible()
+    }
+}

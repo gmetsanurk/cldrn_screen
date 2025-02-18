77aaf22f1242e108e66f75085f376f025077c2d1
Georgy Metsanurk <gmetsanurk@gmail.com>
2025-02-18 09:47:17 +0300

    create AppColors + cells refactor

---
+++ cldrn_screen/AppColors.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+struct AppColors {
+    static let homeScreenBackgroundColor = UIColor.white
+    static let personCellTextColor = UIColor.black
+    static let personCellTextAligmentColor = UIColor.gray
+    static let childCellTextColor = UIColor.black
+    static let childCellTextAligmentColor = UIColor.gray
+}
+
+struct AppGeometry {
+    static let cornerRadius = 10.0
+}
+
